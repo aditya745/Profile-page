f93763cb4539b94f833cbc32a08a2d6d3d5ff37f
aditya745 <thakuraditya72@yahoo.in>
2019-07-17 11:15:40 +0300

    hover effect fixed

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,46 +1,76 @@
-import React, { Component } from "react";
-import SignUpForm from "./SignUpForm";
+import React from 'react'
+import SignUpForm from './SignUpForm'
 
-class Rating extends Component {
+class Rating extends React.Component {
   state = {
-    rating: 2,
+    rating: 3,
+    temp_rating: null,
     showForm: false
-  };
-  rate = rating => {
+  }
+
+  handleForm = () => {
+    console.log('handleform', this.state.showForm)
     this.setState({
-      showForm: true,
-      rating: rating
-    });
-  };
+      showForm: true
+    })
+  }
 
   handleHideForm = () => {
+    console.log('handleHideForm', this.state.showForm)
     this.setState({
       showForm: false
-    });
-  };
+    })
+  }
+  rate = rating => {
+    this.setState({
+      rating: rating,
+      temp_rating: rating
+    })
+  }
+  star_over = rating => {
+    this.setState({
+      rating: rating,
+      temp_rating: rating
+    })
+  }
+  star_out = () => {
+    this.setState({ rating: 3 })
+  }
   render() {
-    var stars = [];
+    console.log('rating', this.state.rating)
+    var stars = []
+
     for (var i = 1; i <= 5; i++) {
-      var star = "starRatingStar";
+      var klass = 'star-rating__star'
+
       if (this.state.rating >= i && this.state.rating != null) {
-        star += " isSelected";
+        klass += ' is-selected'
       }
 
       stars.push(
-        <label key={i} className={star} onClick={this.rate.bind(this, i)}>
+        <label
+          className={klass}
+          onClick={() => {
+            this.rate.bind(this, i)
+            this.handleForm()
+          }}
+          onMouseOver={this.star_over.bind(this, i)}
+          onMouseOut={this.star_out}
+        >
           ★
         </label>
-      );
+      )
     }
-    if (!this.state.showForm) {
-      return <div className="starRating">{stars}</div>;
-    } else {
-      return (
-        <div>
+
+    return (
+      <div className="star-rating">
+        {this.state.showForm ? (
           <SignUpForm handleHideForm={this.handleHideForm} />
-        </div>
-      );
-    }
+        ) : (
+            stars
+          )}
+      </div>
+    )
   }
 }
-export default Rating;
+export default Rating
